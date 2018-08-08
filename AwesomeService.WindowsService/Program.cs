8daae730a2dd3c5fbe03968815cfef28994fe1e6
Shems E. Boukhatem <contact@shemseddine.com>
2018-08-08 19:28:52 +0100

    init

---
+++ AwesomeService.WindowsService/Program.cs
@@ -0,0 +1,17 @@
+﻿using AwesomeService.Core;
+using System;
+
+namespace AwesomeService.WindowsService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var runner = new AppRunner();
+            runner.Start();
+            while (runner.IsRunning)
+            {
+            }
+        }
+    }
+}
